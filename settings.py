47f5467bab6f1bfda6231a28b4d368a630850ee3
Kipi122 <52936972+Kipi122@users.noreply.github.com>
2025-02-12 04:10:56 +0200

    try env vars

--- settings.py
+++ settings.py
@@ -65,10 +65,10 @@ PARTICIPANT_FIELDS = [
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=0.00, participation_fee=2.00, doc=""  # TODO change this to real values
 )
-#OTREE_PRODUCTION = environ.get('OTREE_PRODUCTION')
+OTREE_PRODUCTION = environ.get('OTREE_PRODUCTION')
 OTREE_AUTH_LEVEL = environ.get('OTREE_AUTH_LEVEL') #can be DEMO or STUDY
 OTREE_ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
-DEBUG = 1 #HACK can change to 1 for debugging
+#DEBUG = 1 #HACK can change to 1 for debugging
 LANGUAGE_CODE = 'en'
 REAL_WORLD_CURRENCY_CODE = 'GBP'
 USE_POINTS = True
